510a5437afa85ed45dbfe2bb078b24e5cf1449df
Zayid Ahmed <zayidahamd514@gmail.com>
2026-01-06 05:05:36 +0600

    Move backend files to root for Render

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ mongoose.connect(process.env.MONGO_URI, {
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
 
+
+
+app.use(express.static('public'));
+
 // Import routes
 const authRoute = require('./routes/auth');
 const todoRoute = require('./routes/todo');
